00938076a60ee0cfbfa2d5972410c29453485d21
Nakaeriacrid <abnera.silva@proton.me>
2026-09-17 17:11:40 -0300

    Shell de terminal

---
+++ fish/config.fish
@@ -0,0 +1,4 @@
+fastfetch -c $XDG_CONFIG_HOME/fastfetch/config.jsonc
+alias dotfiles="/usr/bin/git --git-dir='$HOME/Projetos Git/.dotfiles' --work-tree=$XDG_CONFIG_HOME"
+function fish_greeting
+end
